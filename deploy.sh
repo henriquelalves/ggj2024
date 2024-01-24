a4ed38306d49a4654570d48d6a7fe5c810ec5da8
Henrique Alves <henriquelalves@gmail.com>
2024-01-24 23:39:06 +0200

    first deploy

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+readonly GODOT_4_PATH="/home/peronio/.local/bin/Godot_v4.2.1.x86_64"
+readonly PROJECT_NAME="ggj-2024"
+
+mkdir -p ./build/
+${GODOT_4_PATH} --headless --export-release "Web" ./build/build.html
+mv ./build/build.html ./build/index.html
+zip -r -j build.zip ./build/*
+butler push ./build.zip illusion-fisherman/${PROJECT_NAME}:html5
+rm -rf ./build/
+rm ./build.zip
