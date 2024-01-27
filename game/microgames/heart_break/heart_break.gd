2fa482cf3897c02df21e2c6e5ff343f41043037e
Marcelo Costa <marcardion@gmail.com>
2024-01-26 23:34:36 -0300

    Starting HeartBreak Microgame

---
+++ game/microgames/heart_break/heart_break.gd
@@ -0,0 +1,18 @@
+extends Microgame
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _on_player_heart_body_entered(body):
+	if body is Spiky:
+		print("hey")
+		$PlayerHeart.get_node("AnimatedSprite2D").play("death")
+		## GameOver
+		pass
