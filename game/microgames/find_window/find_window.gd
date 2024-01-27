7f4fc3b59c286c7c50df43c4f75eab330955e4f3
Marcelo Costa <>
2024-01-27 16:16:37 -0300

    MVP for Find Window Microgame

---
+++ game/microgames/find_window/find_window.gd
@@ -0,0 +1,18 @@
+extends Microgame
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var windows = $AllWindows.get_children()
+	var max_windows = windows.size() - randi_range(0,2)
+	for i in max_windows:
+		var window = windows[i]
+		window.position = window.position + Vector2(randf_range(-100, 100), randf_range(-100,100))
+		window.show()
+		$Timer.start(0.1)
+		await $Timer.timeout
+
+
+func _on_send_button_button_down():
+	%TextBoxMessage.hide()
+	%ChatMessage.show()
---
+++ game/microgames/find_window/msn_regular.gd
@@ -0,0 +1,6 @@
+extends TextureRect
+
+
+
+func _on_button_button_down():
+	queue_free()
