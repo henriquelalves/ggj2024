36f1a2024be66d89b6adfb07965193dce4e4c9da
Henrique Alves <henrique.alves@itsjungle.xyz>
2024-01-28 16:01:09 +0200

    Fixed flowers delivery

--- game/microgames/flowers_delivery/player.gd
+++ game/microgames/flowers_delivery/player.gd
@@ -1,15 +1,21 @@
 extends AnimatedSprite3D
 
-func _ready() -> void:
-	pass
+signal died
+var _died = false
 
 
 func _process(delta: float) -> void:
-	position.z -= delta * 3
+	if not _died:
+		position.z -= delta * 3
+		
+		if Input.is_anything_pressed():
+			animation = "dodge"
+			offset.y = -100.0
+		else:
+			animation = "default"
+			offset.y = 0.0
 	
-	if Input.is_anything_pressed():
-		animation = "dodge"
-		offset.y = -100.0
-	else:
-		animation = "default"
-		offset.y = 0.0
+		if ($Area3D as Area3D).has_overlapping_areas():
+			if animation == "default":
+				_died = true
+				died.emit()
